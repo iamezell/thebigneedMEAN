409e6bfbd4834ee8f727424cdf9eb31b32d7d8b0
ezell <iamezell@gmail.com>
2015-12-08 08:37:31 -0500

    adding setup details and new tree structure

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,11 @@ gulp.task('jscs', function(){
 	.pipe(jshint.reporter('jshint-stylish',{verbose:true}))
 	.pipe(jshint.reporter('fail'))
 });
+
+gulp.task('js-watch', ['vet'], function(){
+  console.log('something changed');
+  browserSync.reload();
+})
 gulp.task('test', function (done) {
   new Server({
     configFile: __dirname + '/karma.conf.js',
@@ -57,17 +62,32 @@ gulp.task('nodemon', function(cb){
 })
 
 
-gulp.task('vet', ['test','jscs'], function(){
+gulp.task('vet', ['test','jscs','nodemon'], function(){
+	
+	glob('./src/**/js/**.js', function(err, files){
+          if(err) done(err);
 
-	return browserify('./src/js/app.js')
-	.bundle()
+          var tasks = files.map(function(entry){
+            return browserify({entries:[entry]})
+            .bundle()
+            .pipe(source(entry))
+            .pipe(buffer())
+            .pipe(rename({extname:'.bundle.js'}))
+            .pipe(gulp.dest('./public/javascripts'))
+          })
+          es.merge(tasks).on('end', function(){
+            console.log('done');
+          });
+        });
+	//return browserify('./src/js/app.js')
+	//.bundle()
 	//Pass desired output filename to vinyl-source-stream
-    .pipe(source('bundle.js'))
- //    // Start piping stream to tasks!
- 	.pipe(buffer())
+    //.pipe(source('bundle.js'))
+     // Start piping stream to tasks!
+// 	.pipe(buffer())
 	// .pipe(jshint())
 	// .pipe(jshint.reporter('jshint-stylish',{verbose:true}))
 	// .pipe(jshint.reporter('fail'))
 	// // Start piping stream to tasks!
-   .pipe(gulp.dest('./public/javascripts'));
+   //.pipe(gulp.dest('./public/javascripts'));
 });
